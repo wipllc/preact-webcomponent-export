f8002d84317903935b61128976d7ab6ab1de5094
Pedro Sanches <p.gomez.almeida@gmail.com>
2023-08-31 14:04:07 -0400

    Revert "initial quiz"

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,17 @@
 import { defineConfig } from "vite";
-import preact from "@preact/preset-vite";
 import { resolve } from "path";
+import preact from "@preact/preset-vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
+
   build: {
     lib: {
       entry: resolve(__dirname, "src/main.jsx"),
-      name: "AudienceQuiz",
+      name: "MyComponent",
       // the proper extensions will be added
-      fileName: "audience-quiz",
-    },
-    alias: {
-      react: "preact/compat",
+      fileName: "my-component",
     },
   },
 });
